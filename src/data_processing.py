f9ff389e2b1e72f11b0ffef8d48a44bb9a3d6008
betyjo <155064115+betyjo@users.noreply.github.com>
2026-06-01 15:05:04 +0300

    Build preprocessing pipeline

--- src/data_processing.py
+++ src/data_processing.py
@@ -73,3 +73,52 @@ def merge_customer_features(df):
     )
 
     return merged
+def build_preprocessor(
+    numerical_cols,
+    categorical_cols
+):
+        numerical_pipeline = Pipeline(
+        steps=[
+            (
+                "imputer",
+                SimpleImputer(
+                    strategy="median"
+                )
+            ),
+            (
+                "scaler",
+                StandardScaler()
+            )
+        ]
+    )
+        categorical_pipeline = Pipeline(
+        steps=[
+            (
+                "imputer",
+                SimpleImputer(
+                    strategy="most_frequent"
+                )
+            ),
+            (
+                "encoder",
+                OneHotEncoder(
+                    handle_unknown="ignore"
+                )
+            )
+        ]
+    )
+        preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "num",
+                numerical_pipeline,
+                numerical_cols
+            ),
+            (
+                "cat",
+                categorical_pipeline,
+                categorical_cols
+            )
+        ]
+    )
+        return preprocessor
